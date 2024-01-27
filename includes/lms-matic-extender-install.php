96096a76a49a359e783db020fa56eb7681723ed9
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-27 13:20:38 -0500

    add timezone require

--- includes/lms-matic-extender-install.php
+++ includes/lms-matic-extender-install.php
@@ -12,6 +12,7 @@ require_once(LMSCX_ACHIEVEMENTS_PATH_CONFIG . 'Config.php');
 require_once(LMSCX_ACHIEVEMENTS_PATH_MODEL . 'Model.php');
 require_once(LMSCX_ACHIEVEMENTS_PATH_HELPERS . 'Helpers.php');
 require_once(LMSCX_ACHIEVEMENTS_PATH_CPT . 'CPT.php');
+require_once(LMSCX_ACHIEVEMENTS_PATH_TIMEZONE . 'Timezone.php');
 require_once(LMSCX_ACHIEVEMENTS_PATH_METABOXES . 'Metabox.php');
 require_once(LMSCX_ACHIEVEMENTS_PATH_ENQUEUE . 'Enqueue.php');
 require_once(LMSCX_ACHIEVEMENTS_PATH_VIEW . 'View.php');
